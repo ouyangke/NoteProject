670e1a9473c71d219764fa3691aae9d4fe7d422e
oyke <oyke@foxmail.com>
2023-09-13 22:23:45 +0800

    feat:UVCDemo

--- sample/uvcdemo/src/main/java/com/example/uvcdemo/MainActivity.kt
+++ sample/uvcdemo/src/main/java/com/example/uvcdemo/MainActivity.kt
@@ -4,19 +4,28 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
+import android.media.ImageReader
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
-import android.view.SurfaceHolder
-import android.view.SurfaceView
+import android.util.Log
+import android.util.Range
+import android.view.Surface
+import android.view.TextureView
+import android.view.TextureView.SurfaceTextureListener
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,17 +36,21 @@ class MainActivity : AppCompatActivity() {
 
     private var cameraManager: CameraManager? = null
 
-    private lateinit var sv1: SurfaceView
+    private lateinit var sv1: TextureView
+    private lateinit var tv1:TextView
     private var device1: CameraDevice? = null
     private var previewRequestBuilder1: CaptureRequest.Builder? = null
     private var cameraSession1: CameraCaptureSession? = null
+    private var nv21Reader1: ImageReader? = null
     private var handlerThread1: HandlerThread? = null
     private var handler1: Handler? = null
 
-    private lateinit var sv2: SurfaceView
+    private lateinit var sv2: TextureView
+    private lateinit var tv2:TextView
     private var device2: CameraDevice? = null
     private var previewRequestBuilder2: CaptureRequest.Builder? = null
     private var cameraSession2: CameraCaptureSession? = null
+    private var nv21Reader2: ImageReader? = null
     private var handlerThread2: HandlerThread? = null
     private var handler2: Handler? = null
 
@@ -47,17 +60,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sv1 = findViewById(R.id.sv_1)
+        tv1 = findViewById(R.id.tv_fps1)
         sv2 = findViewById(R.id.sv_2)
+        tv2 = findViewById(R.id.tv_fps2)
 
         if (PackageManager.PERMISSION_GRANTED == ActivityCompat
-                .checkSelfPermission(this, Manifest.permission.CAMERA)) {
+                .checkSelfPermission(this, Manifest.permission.CAMERA)
+        ) {
             //已授权
             init()
         } else {
             //未授权
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_CAMERA)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CODE_CAMERA
+            )
         }
-
 
 
     }
@@ -80,35 +99,65 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    private fun init(){
+    private fun init() {
         //初始化相机
         initCamera()
 
-        sv1.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
+        sv1.surfaceTextureListener = object : SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                Log.e(TAG, "sv1:onSurfaceTextureAvailable：$width---$height")
                 openCamera1()
             }
 
-            override fun surfaceChanged(
-                holder: SurfaceHolder, format: Int, width: Int, height: Int
-            ) = Unit
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
 
-            override fun surfaceDestroyed(holder: SurfaceHolder) = Unit
+            }
 
-        })
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return true
+            }
 
-        sv2.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+
+            }
+
+        }
+
+        sv2.surfaceTextureListener = object : SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                Log.e(TAG, "sv2:onSurfaceTextureAvailable：$width---$height")
                 openCamera2()
             }
 
-            override fun surfaceChanged(
-                holder: SurfaceHolder, format: Int, width: Int, height: Int
-            ) = Unit
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
 
-            override fun surfaceDestroyed(holder: SurfaceHolder) = Unit
+            }
 
-        })
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+
+            }
+
+        }
     }
 
     /**
@@ -120,15 +169,36 @@ class MainActivity : AppCompatActivity() {
         cameraManager?.let {
             //获取相机列表
             val cameraIdList = it.cameraIdList
+            for (cameraId in cameraIdList) {
+                val character = cameraManager?.getCameraCharacteristics(cameraId)
+                val ranges =
+                    character?.get(CameraCharacteristics.CONTROL_AE_AVAILABLE_TARGET_FPS_RANGES)
+                Log.e(TAG, "camera${cameraId}支持的FPS范围${Arrays.toString(ranges)}")
+                val map = character?.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                val outputSizes = map?.getOutputSizes(SurfaceTexture::class.java)
+                Log.e(TAG, "camera${cameraId}支持的预览尺寸${Arrays.toString(outputSizes)}")
+            }
 
         }
         handlerThread1 = HandlerThread("handlerThread1")
         handlerThread1?.start()
         handler1 = Handler(Looper.getMainLooper())
+        nv21Reader1 = ImageReader.newInstance(640, 640, ImageFormat.YUV_420_888, 2)
+        nv21Reader1?.setOnImageAvailableListener({
+            val image = it.acquireLatestImage()
+            getFps1()
+            image.close()
+        }, handler1)
 
         handlerThread2 = HandlerThread("handlerThread2")
         handlerThread2?.start()
         handler2 = Handler(Looper.getMainLooper())
+        nv21Reader2 = ImageReader.newInstance(320, 240, ImageFormat.YUV_420_888, 2)
+        nv21Reader2?.setOnImageAvailableListener({
+            val image = it.acquireLatestImage()
+            getFps2()
+            image.close()
+        }, handler2)
     }
 
     /**
@@ -177,17 +247,25 @@ class MainActivity : AppCompatActivity() {
     private fun createCameraPreviewSession1() {
         try {
             previewRequestBuilder1 = device1?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            val previewSurface1 = sv1.holder.surface
+            previewRequestBuilder1?.set(
+                CaptureRequest.CONTROL_AF_MODE,
+                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+            )
+            previewRequestBuilder1?.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, Range(30,30))
+            val surfaceTexture1 = sv1.surfaceTexture
+            surfaceTexture1?.setDefaultBufferSize(640, 640)
+            val previewSurface1 = Surface(surfaceTexture1)
+
             previewRequestBuilder1?.addTarget(previewSurface1)
+            nv21Reader1?.surface?.let { previewRequestBuilder1?.addTarget(it) }
+
             device1?.createCaptureSession(
-                mutableListOf(previewSurface1), object : CameraCaptureSession.StateCallback() {
+                mutableListOf(previewSurface1, nv21Reader1?.surface),
+                object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
                         try {
                             cameraSession1 = session
-                            previewRequestBuilder1?.set(
-                                CaptureRequest.CONTROL_AF_MODE,
-                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
-                            )
+
                             val captureRequest1: CaptureRequest? = previewRequestBuilder1?.build()
                             if (captureRequest1 != null) {
                                 cameraSession1?.setRepeatingRequest(captureRequest1, null, handler1)
@@ -200,7 +278,8 @@ class MainActivity : AppCompatActivity() {
                     override fun onConfigureFailed(session: CameraCaptureSession) {
 
                     }
-                }, handler1
+                },
+                handler1
             )
         } catch (e: CameraAccessException) {
             e.printStackTrace()
@@ -210,10 +289,14 @@ class MainActivity : AppCompatActivity() {
     private fun createCameraPreviewSession2() {
         try {
             previewRequestBuilder2 = device2?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            val previewSurface2 = sv2.holder.surface
+            previewRequestBuilder2?.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, Range(30,30))
+            val surfaceTexture2 = sv2.surfaceTexture
+            surfaceTexture2?.setDefaultBufferSize(320, 240)
+            val previewSurface2 = Surface(surfaceTexture2)
             previewRequestBuilder2?.addTarget(previewSurface2)
+            nv21Reader2?.surface?.let { previewRequestBuilder2?.addTarget(it) }
             device2?.createCaptureSession(
-                mutableListOf(previewSurface2), object : CameraCaptureSession.StateCallback() {
+                mutableListOf(previewSurface2,nv21Reader2?.surface), object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
                         try {
                             cameraSession2 = session
@@ -239,4 +322,33 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+
+    private var mLastTime1: Long = 0
+    private var fps1 = 0
+    private var ifps1: Int = 0
+    private fun getFps1() {
+        val now = System.currentTimeMillis()
+        ifps1++
+        if (now > mLastTime1 + 1000) {
+            mLastTime1 = now
+            fps1 = ifps1
+            tv1.text = "fps1:$fps1"
+            ifps1 = 0
+        }
+    }
+
+    private var mLastTime2: Long = 0
+    private var fps2 = 0
+    private var ifps2: Int = 0
+    private fun getFps2() {
+        val now = System.currentTimeMillis()
+        ifps2++
+        if (now > mLastTime2 + 1000) {
+            mLastTime2 = now
+            fps2 = ifps2
+            tv2.text = "fps2:$fps2"
+            ifps2 = 0
+        }
+    }
+
 }
